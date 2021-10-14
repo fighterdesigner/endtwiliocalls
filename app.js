58b588fb9423f347a1065ecec27ebe5d72bd47ee
fighterdesigner <designsleader@gmail.com>
2021-10-14 10:51:04 +0000

    update the end of a call

--- app.js
+++ app.js
@@ -1,46 +1,48 @@
-require("dotenv").config()
-const accountSid = process.env.ACCOUNT_SID
-const authToken = process.env.AUTH_TOKEN
-const url = process.env.URL
-const client = require('twilio')(accountSid, authToken)
-const cron = require('node-cron')
-const axios = require('axios')
+require("dotenv").config();
+const accountSid = process.env.ACCOUNT_SID;
+const authToken = process.env.AUTH_TOKEN;
+const url = process.env.URL;
+const client = require("twilio")(accountSid, authToken);
+const cron = require("node-cron");
+const axios = require("axios");
 
-cron.schedule('* * * * * *', () => {
-  getCalls()
-})
+cron.schedule("*/10 * * * * * *", () => {
+  getCalls();
+});
 
 const getCalls = () => {
   axios(url)
-  .then(data => {
-    if(data.data['hydra:member'].length !== 0) {
-    loopThroughCalls(data.data['hydra:member'])
-    }
-  })
-  .catch(err => console.log(err))
-}
+    .then(data => {
+      loopThroughCalls(data.data["hydra:member"]);
+    })
+    .catch(err => console.log(err));
+};
 
-const endCalls = (callsid) => {
-  client.calls(callsid).update({status: 'completed'})
-}
+const endCalls = (callsid, consumedFreeTime) => {
+  //client.calls(callsid).update({ status: "completed" });
+  client.calls(callsid).update({ twiml: "<Response><Hangup/></Response>" });
+
+  //TODO : Mettre a jour le statut de label sur la base de donnees 'termine'
+};
 
 const loopThroughCalls = (calls) => {
-  while(calls.length !== 0) {
-    for(let call of calls) {
-      const proFreeTime = call.proFreeMinutesNumber * 60
-      const newDate = new Date(call.date)
-      if(dateDiff(newDate) >= proFreeTime) {
-      console.log(dateDiff(newDate))
-      console.log(call.twilioCallSid)
-      endCalls(call.twilioCallSid)
-      }
+  console.log(
+    `${calls.length} calls en attente with freeCall found ${new Date()}`
+  );
+  for (let call of calls) {
+    const proFreeTime = call.proFreeMinutesNumber * 60;
+    const newDate = new Date(call.date);
+    const diff = dateDiff(newDate);
+    if (diff >= proFreeTime) {
+      console.log(
+        `Cons/Free : ${diff}/${proFreeTime} sec, Call at : ${newDate}, Call Sid : ${call.twilioCallSid}`
+      );
+      endCalls(call.twilioCallSid, diff);
     }
-    calls.length = 0
   }
-}
+};
 
 const dateDiff = (date) => {
-  const currentDate = new Date()
+  const currentDate = new Date();
   return (currentDate.getTime() - date.getTime()) / 1000;
-}
-
+};
